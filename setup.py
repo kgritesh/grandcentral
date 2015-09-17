e05e4f4c3a9296bece5734ff21f7b643ea85eb15
Ritesh Kadmawala <ritesh@voxapp.com>
2015-09-17 13:44:45 +0530

    - Updated package name to grandcentral-py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 ]
 
 setup(
-    name='grandcentral',
+    name='grandcentral-py',
     version='0.1.0',
     description="GrandCentral is an extremely basic publisher subscriber event library",
     long_description=readme + '\n\n' + history,
